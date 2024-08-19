45bf249f4584b74c4df56bee1380431c97a5c69b
perpel <572821520@qq.com>
2024-08-19 12:57:03 +0800

    update

---
+++ helper.go
@@ -0,0 +1,67 @@
+package goframework_gorm_mysql
+
+import (
+	"github.com/kordar/godb"
+	log "github.com/kordar/gologger"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	mysqlpool  = godb.NewDbPool()
+	dbLogLevel = "info"
+)
+
+func SetDbLogLevel(level string) {
+	dbLogLevel = level
+}
+
+func GetMysqlDB(db string) *gorm.DB {
+	return mysqlpool.Handle(db).(*gorm.DB)
+}
+
+func gormConfig() *gorm.Config {
+	mysqlConfig := gorm.Config{}
+	if dbLogLevel == "error" {
+		mysqlConfig.Logger = logger.Default.LogMode(logger.Error)
+	}
+	if dbLogLevel == "warn" {
+		mysqlConfig.Logger = logger.Default.LogMode(logger.Warn)
+	}
+	if dbLogLevel == "info" {
+		mysqlConfig.Logger = logger.Default.LogMode(logger.Info)
+	}
+	return &mysqlConfig
+}
+
+// AddMysqlInstances 批量添加mysql句柄
+func AddMysqlInstances(dbs map[string]map[string]string) {
+	for db, cfg := range dbs {
+		err := AddMysqlInstance(db, cfg)
+		if err != nil {
+			log.Warnf("[godb-mysql] 初始化异常，err=%v", err)
+		}
+	}
+
+}
+
+// AddMysqlInstance 添加mysql句柄
+func AddMysqlInstance(db string, cfg map[string]string) error {
+	ins := NewGormConnIns(db, cfg, gormConfig())
+	return mysqlpool.Add(ins)
+}
+
+func AddMysqlInstanceWithDsn(db string, dsn string) error {
+	ins := NewGormConnInsWithDsn(db, dsn, gormConfig())
+	return mysqlpool.Add(ins)
+}
+
+// RemoveMysqlInstance 移除mysql句柄
+func RemoveMysqlInstance(db string) {
+	mysqlpool.Remove(db)
+}
+
+// HasMysqlInstance mysql句柄是否存在
+func HasMysqlInstance(db string) bool {
+	return mysqlpool != nil && mysqlpool.Has(db)
+}
--- mysql.go
+++ mysql.go
@@ -1,71 +1,43 @@
 package goframework_gorm_mysql
 
 import (
-	"github.com/kordar/godb"
+	"fmt"
 	log "github.com/kordar/gologger"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
-var (
-	mysqlpool  = godb.NewDbPool()
-	dbLogLevel = "info"
-)
-
-func SetDbLogLevel(level string) {
-	dbLogLevel = level
+type GormConnIns struct {
+	name string
+	ins  *gorm.DB
 }
 
-func GetMysqlDB(db string) *gorm.DB {
-	return mysqlpool.Handle(db).(*gorm.DB)
+func NewGormConnIns(name string, cfg map[string]string, config *gorm.Config) *GormConnIns {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", cfg["user"], cfg["password"], cfg["host"], cfg["port"], cfg["db"], "charset="+cfg["charset"]+"&parseTime=true")
+	return NewGormConnInsWithDsn(name, dsn, config)
 }
 
-func gormConfig() *gorm.Config {
-	mysqlConfig := gorm.Config{}
-	if dbLogLevel == "error" {
-		mysqlConfig.Logger = logger.Default.LogMode(logger.Error)
+func NewGormConnInsWithDsn(name string, dsn string, config *gorm.Config) *GormConnIns {
+	ins, err := gorm.Open(mysql.Open(dsn), config)
+	if err != nil {
+		log.Errorf("[godb-mysql] 初始化mysql失败,dsn=%s,err=%v", dsn, err)
+		return nil
 	}
-	if dbLogLevel == "warn" {
-		mysqlConfig.Logger = logger.Default.LogMode(logger.Warn)
+	return &GormConnIns{name: name, ins: ins}
+}
+
+func (c GormConnIns) GetName() string {
+	return c.name
+}
+
+func (c GormConnIns) GetInstance() interface{} {
+	return c.ins
+}
+
+func (c GormConnIns) Close() error {
+	if s, err := c.ins.DB(); err == nil {
+		return s.Close()
+	} else {
+		return err
 	}
-	if dbLogLevel == "info" {
-		mysqlConfig.Logger = logger.Default.LogMode(logger.Info)
-	}
-	return &mysqlConfig
-}
-
-// InitMysqlHandle 初始化mysql句柄
-func InitMysqlHandle(dbs map[string]map[string]string) {
-	for db, cfg := range dbs {
-		ins := NewGormConnIns(db, cfg, gormConfig())
-		if ins == nil {
-			continue
-		}
-		err := mysqlpool.Add(ins)
-		if err != nil {
-			log.Warnf("初始化异常，err=%v", err)
-		}
-	}
-
-}
-
-// AddMysqlInstance 添加mysql句柄
-func AddMysqlInstance(db string, cfg map[string]string) error {
-	ins := NewGormConnIns(db, cfg, gormConfig())
-	return mysqlpool.Add(ins)
-}
-
-func AddMysqlInstanceWithDsn(db string, dsn string) error {
-	ins := NewGormConnInsWithDsn(db, dsn, gormConfig())
-	return mysqlpool.Add(ins)
-}
-
-// RemoveMysqlInstance 移除mysql句柄
-func RemoveMysqlInstance(db string) {
-	mysqlpool.Remove(db)
-}
-
-// HasMysqlInstance mysql句柄是否存在
-func HasMysqlInstance(db string) bool {
-	return mysqlpool != nil && mysqlpool.Has(db)
 }
